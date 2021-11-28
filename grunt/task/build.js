5c243ba6971c67ad1cc0f88b15ccfe0100e9f4d7
Tim Doerzbacher <tim@tim-doerzbacher.com>
2021-11-28 16:48:06 -0500

    Update PDF rendering to use PagedJS

--- grunt/task/build.js
+++ grunt/task/build.js
@@ -1,14 +1,52 @@
 'use strict';
 
+const execSync = require("child_process").execSync;
+const fs = require('fs');
+const handlebars = require('handlebars');
+const yaml = require('js-yaml');
+
+handlebars.registerHelper('cleanJunk', function (input) {
+  return input.replace(/(¥|https:\/\/)/g, '');
+});
+
+function registerPartial(partialName, filename) {
+  handlebars.registerPartial(partialName, fs.readFileSync(filename, 'utf8'));
+}
+
+registerPartial('contactInfoItem', 'src/partial/contactInfoItem.html.tmpl');
+registerPartial('sidebarListItem', 'src/partial/sidebarListItem.html.tmpl');
+registerPartial('accomplishmentItem', 'src/partial/accomplishmentItem.html.tmpl');
+registerPartial('workHistoryItem', 'src/partial/workHistoryItem.html.tmpl');
+
 module.exports = function (grunt) {
   grunt.registerTask('build:mkdir', function () {
     grunt.file.mkdir('build/');
   });
+
+  grunt.registerTask(
+    'build:html',
+    'Renders resume template to HTML',
+    function () {
+      var context = yaml.load(fs.readFileSync('src/resume.yml', 'utf8'));
+      // next, let's add the git revision, but I only care about the first
+      // 12 characters.
+      context.head = execSync('git rev-parse HEAD', {'encoding': 'utf8'}).trim().substr(0, 12);
+      // creation date also seems like a useful thing
+      var now = new Date();
+      context.created = now.toISOString().substr(0, 10);
+      // load the template
+      var templateSource = fs.readFileSync('src/resume.html.tmpl', 'utf8');
+      var template = handlebars.compile(templateSource);
+      // lastly, render it out and save it
+      var rendered = template(context);
+      fs.writeFileSync('build/resume.html', rendered);
+    });
 
   grunt.registerTask('build', [
     'clean',
     'copy',
     'build:mkdir',
     'sass',
+    'build:html',
   ]);
 };
--- grunt/task/generate.js
+++ grunt/task/generate.js
@@ -1,54 +1,13 @@
 'use strict';
 
-const puppeteer = require('puppeteer');
-const grunt = require('grunt');
-
-async function createPDF(done, filename) {
-  grunt.log.writeln('Creating browser');
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-
-  grunt.log.writeln('Loading resume');
-  await page.goto('http://localhost:8080', {
-    // see https://github.com/puppeteer/puppeteer/blob/v1.11.0/docs/api.md for
-    // more options.
-    waitUntil: 'networkidle0',
-  });
-  // puppeteer automatically defaults to A3, which isn't what I ever want.
-  // Going w/ auto seems to make things work the same on the command line
-  // and inside the browser.
-  await page.addStyleTag({
-    content: '@page { size: auto; }',
-  });
-
-  grunt.log.writeln('Saving PDF');
-  var pdf = await page.pdf({
-    path: filename,
-    format: 'Letter',
-    printBackground: true,
-    margin: {
-      top: '10mm',
-      right: '15mm',
-      bottom: '10mm',
-      left: '15mm',
-    },
-  });
-
-  await browser.close();
-
-  done();
-  return pdf;
-}
-
 module.exports = function (grunt) {
   grunt.registerTask('generate:pdf', function () {
-    var done = this.async();
-    createPDF(done, 'resume.pdf');
+    const execSync = require("child_process").execSync;
+    execSync('npx pagedjs-cli build/resume.html -o resume.pdf');
   });
 
   grunt.registerTask('generate', [
-    'server:start',
+    'build',
     'generate:pdf',
-    'server:stop',
   ]);
 };
